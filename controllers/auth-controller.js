4f6b772ea51e88d29eecd9759e5d91e2aea958e6
phongphat <phongphat.jap@gmail.com>
2025-02-05 11:08:47 +0700

    update

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,13 @@
 exports.register = (req, res, next) => {
   try {
     // code
+    // Step 1 req.body
+    console.log(req.body);
+    // Step 2 validate
+    // Step 3 Check already
+    // Step 4 Encrypt bcrypt
+    // Step 5 Insert to DB
+    // Step 6 Response
     res.json({ message: "hello register " });
   } catch (error) {
     next(error);
